3a7e7ea29d50aa7544875ad3bcf1bbc50b7a567b
anoopsethu <anoop4sethu@gmail.com>
2022-01-19 21:45:40 +0530

    first commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
   var swatchUp, swatchDown
   var fillSpeed = 0.15
   var chosenColor = '#FFFFFF'
-  var colors = ['#FFFFFF', '#8E53A1', '#6ABD46', '#71CCDC', '#F7ED45', '#F7DAAF', '#EC2527', '#F16824', '#CECCCC', '#5A499E', '#06753D', '#024259', '#FDD209', '#7D4829', '#931B1E', '#B44426', '#979797', '#C296C5', '#54B948', '#3C75BB', '#F7ED45', '#E89D5E', '#F26F68', '#F37123', '#676868', '#9060A8', '#169E49', '#3CBEB7', '#FFCD37', '#E5B07D', '#EF3C46', '#FDBE17', '#4E4D4E', '#6B449B', '#BACD3F', '#1890CA', '#FCD55A', '#D8C077', '#A62E32', '#F16A2D', '#343433', '#583E98', '#BA539F', '#9D2482', '#DD64A5', '#DB778D', '#EC4394', '#E0398C', '#68AF46', '#4455A4', '#FBEE34', '#AD732A', '#D91E36', '#F99B2A']
+  var colors = ['#FFFFFF', '#8E53A1', '#6ABD46', '#71CCDC', '#F7ED45', '#F7DAAF', '#EC2527', '#F16824', '#CECCCC', '#5A499E', '#06753D', '#024259', '#FDD209', '#7D4829', '#931B1E', '#B44426', '#979797', '#C296C5', '#54B948', '#3C75BB', '#F7ED45', '#E89D5E', '#F26F68', '#F37123', '#676868', '#9060A8', '#169E49', '#3CBEB7', '#FFCD37', '#E5B07D', '#EF3C46', '#FDBE17', '#4E4D4E', '#6B449B', '#BACD3F', '#1890CA', '#FCD55A', '#D8C077', '#A62E32', '#F16A2D', '#343433', '#583E98', '#BA539F', '#9D2482', '#DD64A5', '#DB778D', '#EC4394', '#E0398C', '#68AF46', '#4455A4', '#FBEE34', '#AD732A', '#D91E36', '#F99B2A', '#0093E5', '#FF4990']
   var closeOffset
 
   function swatchClick(){
@@ -75,6 +75,11 @@
     $.each(colors, function() {
       var swatch = $('<li/>').appendTo(swatchHolder)
       $(swatch).css('background-color', this)
+      if(this=="#FFFFFF"){
+        $(swatch).css('border', '1px solid #aaa');
+      } else{
+        $(swatch).css('border', '1px solid transparent');
+      }
       $(swatch).data('color', this)
       $(swatch).on('click', swatchClick)
       $(swatch).on('mouseenter mouseleave', colorRollover)
